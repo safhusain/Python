984f1fcf6a403b20cba2191a9635f556f2acc4e2
Safdar Husain Indorewala <saf1234@gmail.com>
2022-11-10 19:14:59 +0000

    First Commit

---
+++ python/Matplotlib/pp.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from numpy import random
+
+import numpy as np
+
+xpoints = np.array([1, 6])
+ypoints = np.array([1, 250])
+
+##plt.plot(xpoints, ypoints,'o:r') ##draw Dashed RED line with circle at the tip.
+
+
+##plt.scatter(xpoints, ypoints) ## Plots scatter points
+##plt.bar(['A','B'],xpoints )
+
+
+##x = np.random.normal(170, 10, 250) ## create normal distribution with 170 mean, 10 SD and 250 values.
+##plt.hist(x)
+
+##sns.distplot(random.normal(size=1000), hist=False)
+
+plt.show()
+
